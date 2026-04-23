73930cb59ac851eb5d8d8be1b1fb7dbc297b1b2f
victormosqueraconejo <victormanuelmosqueraconejo@gmail.com>
2026-04-22 22:28:38 -0500

    components

--- app/src/main/java/com/example/focustimermvvm/presentation/components/AutoResizedText.kt
+++ app/src/main/java/com/example/focustimermvvm/presentation/components/AutoResizedText.kt
@@ -1,4 +1,41 @@
 package com.example.focustimermvvm.presentation.components
 
-class AutoResizedText {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AutoResizedText (
+    modifier: Modifier = Modifier,
+    text : String,
+    textStyle : TextStyle = MaterialTheme.typography.displayLarge
+) {
+
+    var timeTextStyle by remember { mutableStateOf(textStyle) }
+    var fontSizeFactor = 0.95
+
+    Text(
+        text,
+        modifier = modifier.fillMaxWidth(),
+        softWrap = false,
+        style = timeTextStyle,
+        onTextLayout = {
+            if (it.didOverflowWidth) {
+               timeTextStyle = timeTextStyle.copy(
+                   fontSize = timeTextStyle.fontSize * fontSizeFactor
+               )
+            }
+        }
+    )
+
+
 }
+
